f151546a36459fe66e7148031a7b5c05e2d325a5
Anh-Vinh <anhvinh18364@gmail.com>
2025-08-30 17:21:19 +0700

    Combine analyzer, validator and xml agents to the graph

--- main.py
+++ main.py
@@ -1,42 +1,44 @@
-from AnalysistReactAgent.graphBuilder import graph
-from AnalysistReactAgent.State import State
-from XMLAgent.uml_agent import UMLXMLAgent
+import utils
+
+from Agents.XMLAgent.uml_agent import UMLXMLAgent
+from Graph.GraphBuilder import graph
+from Graph.State import State
+
+from dotenv import load_dotenv
+from langgraph.graph import StateGraph, START, END
+
+load_dotenv()
 
 if __name__ == "__main__":
-    spec = """
-    The system is an E-Commerce Platform.
+    required = [
+        "specification",
+        "actors",
+        "usecases",
+        "relationships",
+        "missing_actors",
+        "missing_usecases",
+        "missing_relationships",
+        "invalid_relationships",
+        "wrong_actors",
+        "wrong_targets",
+    ]
 
-    Guests can browse products and must register before logging in.  
-    Users can log in, search for products, add items to the shopping cart, and place orders.  
-    Placing an order requires the user to log in and have at least one item in the cart.  
-    Users can also write product reviews after purchasing an item.  
+    # Reading specification
+    spec_file = "spec.txt"
+    spec = utils.read_spec(spec_file)
 
-    Premium Users, who are a specialized type of User, can access exclusive deals and faster shipping options.  
-
-    Sellers can list new products, update product details, and manage their inventory. They must log in before performing these actions.  
-
-    DeliveryStaff are responsible for shipping orders, updating delivery status, and confirming delivery.  
-
-    Administrators oversee the entire system. They can approve or block sellers, manage users, and handle disputes.  
-    Administrators must log in before performing any action.  
-
-    SupportAgents can assist users by answering queries and resolving complaints. Their ability to resolve complaints depends on the complaint submission by users.  
-
-    All reviews must be linked to a specific product, and complaints must be linked to a specific order.
-    """
-
-    # Bước 1: chạy graph để phân tích spec
-    analyzed_state: State = graph.invoke({"specification": spec})
-
-    # Bước 2: chuyển sang UML Agent
-    uml_state: State = {
-        "actors": analyzed_state.get("actors", []),
-        "usecases": analyzed_state.get("usecases", []),
-        "relationships": analyzed_state.get("relationships", [])
+    # Intialize State
+    test_state: State = {
+        "specification": spec,
     }
 
-    agent = UMLXMLAgent(uml_state)
+    result_state = graph.invoke(test_state)
+    
+    for key in result_state.keys():
+        print(f'{key}: {result_state[key]}')
+        print()
+
+    agent = UMLXMLAgent(result_state)
     xml_str = agent.run()
 
-    print(xml_str)
     agent.save("uml.xml")
